b30b6940fbf01302a3bf4f069de1e841b29404f4
paigess <kwesiboateng777@gmail.com>
2025-08-11 06:57:53 -0400

    movie details

--- js/script.js
+++ js/script.js
@@ -99,48 +99,50 @@ async function displayMovieDetails() {
   div.innerHTML = `
    <div class="details-top">
           <div>
-            <img
+           <a href="movie-details.html?id=${movie.id}">
+            ${
+              movie.poster_path
+                ? `<img
+            src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+              class="card-img-top"
+              alt="${movie.title}"
+            />`
+                : `<img
               src="images/no-image.jpg"
               class="card-img-top"
-              alt="Movie Title"
-            />
+              alt="${movie.title}"
+            />`
+            }
           </div>
           <div>
-            <h2>Movie Title</h2>
+            <h2>${movie.title}</h2>
             <p>
               <i class="fas fa-star text-primary"></i>
-              8 / 10
+             ${movie.vote_average.toFixed(1)}/ 10
             </p>
-            <p class="text-muted">Release Date: XX/XX/XXXX</p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
-              atque molestiae error debitis provident dolore hic odit, impedit
-              sint, voluptatum consectetur assumenda expedita perferendis
-              obcaecati veritatis voluptatibus. Voluptatum repellat suscipit,
-              quae molestiae cupiditate modi libero dolorem commodi obcaecati!
-              Ratione quia corporis recusandae delectus perspiciatis consequatur
-              ipsam. Cumque omnis ad recusandae.
+            <p class="text-muted">Release Date: ${movie.release_date}</p>
+            <p>${movie.overview}
             </p>
             <h5>Genres</h5>
             <ul class="list-group">
-              <li>Genre 1</li>
-              <li>Genre 2</li>
-              <li>Genre 3</li>
+              ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
             </ul>
-            <a href="#" target="_blank" class="btn">Visit Movie Homepage</a>
+            <a href="${movie.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
           </div>
         </div>
         <div class="details-bottom">
           <h2>Movie Info</h2>
           <ul>
-            <li><span class="text-secondary">Budget:</span> $1,000,000</li>
-            <li><span class="text-secondary">Revenue:</span> $2,000,000</li>
-            <li><span class="text-secondary">Runtime:</span> 90 minutes</li>
-            <li><span class="text-secondary">Status:</span> Released</li>
+            <li><span class="text-secondary">Budget:</span> $${movie.budget}</li>
+            <li><span class="text-secondary">Revenue:</span> $${movie.revenue}</li>
+            <li><span class="text-secondary">Runtime:</span> ${movie.runtime} minutes</li>
+            <li><span class="text-secondary">Status:</span> ${movie.status}</li>
           </ul>
           <h4>Production Companies</h4>
           <div class="list-group">Company 1, Company 2, Company 3</div>
         </div>`;
+
+  document.querySelector('#movie-details').appendChild(div);
 }
 
 // FETCH DATA FROM TMDB API
